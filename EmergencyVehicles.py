7862ff808b921fd3a52f934389828cda4572df51
adrian6912 <adrian6912@gmail.com>
2019-04-29 10:47:45 -0500

    Add files via upload

--- EmergencyVehicles.py
+++ EmergencyVehicles.py
@@ -1,4 +1,4 @@
-class Vehicle:
+class Vehicle(object):
     def __init__(self, ID=0, vType=0, zipcode=00000, availability=True):
         self.ID = ID
         self.vType = vType
@@ -9,15 +9,103 @@ class Vehicle:
         self.zipcode = newZipcode
         return
 
-class Request:
+class Request(object):
     def __init__(self, ID, vType, zipcode):
         self.ID = ID
         self.vType = vType
         self.zipcode = zipcode
     
-class Distance:
+class Distance(object):
     def __init__(self, zip1, zip2, distance):
         self.zip1 = zip1
         self.zip2 = zip2
         self.distance = distance
+    
+class EmerVehicles(object):
+    def __init__(self, emergencyVehicles, distances, requests):
+        emergencyVehiclesFile = open(emergencyVehicles)
+        distancesFile = open(distances)
+        requestsFile = open(requests)
+        self.vehicles = self.getVehicles(emergencyVehiclesFile)
+        self.distances = self.getDistances(distancesFile)
+        self.requests = self.getRequest(requestsFile)
+        
+        
+    def processRequests(self):
+        for r in self.requests:
+            j = self.findClosest(r)
+            closest, dist = j[0], j[1]
+            print(r.ID, r.vType, r.zipcode, closest.ID, dist)
+            closest.relocate(r.zipcode)
+        
+    def getVehicles(self, file):
+        '''Returns list of vehicles from provided file'''
+        vehicleList = []
+        for vehicle in file:
+            info = vehicle.split()
+            try:
+                vehicleList.append(Vehicle(int(info[0]), int(info[1]), int(info[2])))
+            except IndexError:
+                pass
+        return vehicleList
+        
+    def getDistances(self, file):
+        '''Returns list of distances from provided file'''
+        distanceList = []
+        for distance in file:
+            info = distance.split()
+            try:
+                distanceList.append(Distance(int(info[0]), int(info[1]), int(info[2])))
+            except IndexError:
+                pass
+        return distanceList
+            
+    def getRequest(self, file):
+        '''Returns list of requests from provided file'''
+        requestList = []
+        for request in file:
+            info = request.split()
+            try:
+                requestList.append(Request(int(info[0]), int(info[1]), int(info[2])))
+            except IndexError:
+                    pass
+        return requestList
+    
+    def findClosest(self, request):
+        '''Returns closest vehicles to given request'''
+        lowestDist = Vehicle()
+        for vehicle in self.vehicles:
+            if vehicle.vType == request.vType:
+                newDist = self.calcDist(vehicle.zipcode, request.zipcode)
+                oldDist = self.calcDist(lowestDist.zipcode, request.zipcode)
+                if (newDist < oldDist):
+                    lowestDist = vehicle
+                if (self.calcDist(lowestDist.zipcode, request.zipcode)) == 0:
+                    return [lowestDist, 0]
+        finalDistance = self.calcDist(lowestDist.zipcode, request.zipcode)
+        return [lowestDist, finalDistance]
+    
+    
+    def calcDist(self, location1, location2):
+        '''Returns the distance between two zipcodes'''
+        from copy import copy
+        if (location1 == 00000 or location2 == 00000):
+            return 500
+        elif location1 == location2:
+            return 0
+        elif location1 > location2:
+            temp = copy(location2)
+            location2 = copy(location1)
+            location1 = copy(temp)
+        distance = 0
+        t = copy(location1)
+        for loc in self.distances:
+            if loc.zip1 == t:
+                if loc.zip2 == location2:
+                    distance = distance + loc.distance
+                    return distance
+                distance = distance + loc.distance
+                t = copy(loc.zip2)
+    
+    
     
--- main.py
+++ main.py
@@ -3,91 +3,11 @@ Created on Apr 19, 2019
 
 @author: Adrian Ridder
 '''
-
-from EmergencyVehicles import Vehicle as V
-from EmergencyVehicles import Request as R
-from EmergencyVehicles import Distance as D
-from copy import copy
-
-def getVehicles():
-    vehicleList = []
-    file = open("EmerVehicles.txt")
-    for vehicle in file:
-        info = vehicle.split()
-        try:
-            vehicleList.append(V(int(info[0]), int(info[1]), int(info[2])))
-        except IndexError:
-            pass
-    return vehicleList
-
-def getRequest():
-    requestList = []
-    file = open("Requests.txt")
-    for request in file:
-        info = request.split()
-        try:
-            requestList.append(R(int(info[0]), int(info[1]), int(info[2])))
-        except IndexError:
-            pass
-    return requestList
-
-def getDistances():
-    distanceList = []
-    file = open("Distance.txt")
-    for distance in file:
-        info = distance.split()
-        try:
-            distanceList.append(D(int(info[0]), int(info[1]), int(info[2])))
-        except IndexError:
-            pass
-    return distanceList
-
-def calcDist(location1, location2):
-    if (location1 == 00000 or location2 == 00000):
-        return 500
-    elif location1 == location2:
-        return 0
-    elif location1 > location2:
-        temp = copy(location2)
-        location2 = copy(location1)
-        location1 = copy(temp)
-    distance = 0
-    t = copy(location1)
-    for loc in distances:
-        if loc.zip1 == t:
-            if loc.zip2 == location2:
-                distance = distance + loc.distance
-                return distance
-            distance = distance + loc.distance
-            t = copy(loc.zip2)
-                
-def findClosest(request):
-    lowestDist = V()
-    for vehicle in vehicles:
-        if vehicle.vType == request.vType:
-            newDist = calcDist(vehicle.zipcode, request.zipcode)
-            oldDist = calcDist(lowestDist.zipcode, request.zipcode)
-            if (newDist < oldDist):
-                lowestDist = vehicle
-            if (calcDist(lowestDist.zipcode, request.zipcode)) == 0:
-                return [lowestDist, 0]
-    finalDistance = calcDist(lowestDist.zipcode, request.zipcode)
-    return [lowestDist, finalDistance]
-
+from EmergencyVehicles import EmerVehicles as EV
 
 if __name__ == '__main__':
-    vehicles = getVehicles()
-    requests = getRequest()
-    distances = getDistances()
-    for r in requests:
-        j = findClosest(r)
-        closest, dist = j[0], j[1]
-        print(r.ID, r.vType, r.zipcode, closest.ID, dist)
-        closest.relocate(r.zipcode)
-
-
-
-
+    thing = EV("EmerVehicles.txt", "Distance.txt", "Requests.txt")
+    thing.processRequests()
 
 
 
